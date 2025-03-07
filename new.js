733f77d5cfe0b2535f27c634c06262812f17c12d
bhavya <bhavyajoshi324@gmail.com>
2025-03-07 12:51:08 +0530

    commit

--- new.js
+++ new.js
@@ -6,3 +6,13 @@ for (let i = 0; i < 10; i++) {
     arr.push(a);
 }
 console.log(arr);
+Ministry of Home and Affriars
+Ministry of finance
+ministry of education
+ministry of labour 
+mibnistry of it & electronics
+ministry of transport
+ ministry of rural development
+ ministry of defence ministry of agriculture
+    ministry of health
+    ministry of agriculture
